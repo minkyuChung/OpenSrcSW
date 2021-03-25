8e166d83993be0ec073ab4505f5b8b71c8acd2c0
minkyu <mink91j@gmail.com>
2021-03-25 18:00:03 +0900

    updated main class

--- src/project/kuir.java
+++ src/project/kuir.java
@@ -5,17 +5,20 @@ public class kuir {
 		
 		if(args!=null) {
 			if(args[0].equals("c")) {
-				makeCollection col = new makeCollection(args[1]);		//"src/html/"
+				makeCollection col = new makeCollection(args[1]);		// "src/html/"
 				col.callDocument();
 				col.convertHTMLtoXML();
 				col.printXML();
 			}else if(args[0].equals("k")) {
-				makeKeyword key = new makeKeyword(args[1]);
+				makeKeyword key = new makeKeyword(args[1]);				// ../collection.xml
 				key.countKeyword();
 				key.printXML();
+			}else if(args[0].equals("i"));{
+				indexer idx = new indexer(args[1]);						// ../index.xml
+				idx.saveKeyword();
+				idx.calculateTfIdf();
 			}
 		}
-
 	}
 }
 
