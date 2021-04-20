73aca2537b84c902d7f8f39ce82339124e581388
minkyu <mink91j@gmail.com>
2021-04-20 18:55:07 +0900

    midterm

---
+++ src/project/genSnippet.java
@@ -0,0 +1,62 @@
+package project;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+
+
+public class genSnippet {
+	File file = null;
+	String query = "";
+	
+	
+	public genSnippet(String filePath, String query) {
+		file = new File(filePath);
+		this.query = query;
+	}
+	
+	public String[] split(String query) {
+		String[] temp = query.split(query);
+		return temp;
+	}
+	
+	public void checkKeyword() throws IOException {
+		BufferedReader br = null;
+		InputStreamReader isr = null;
+		FileInputStream fis = null;
+		String[] keyword = split(query);
+		String[] temp = new String[5];
+		try {
+			fis = new FileInputStream(file);
+			isr = new InputStreamReader(fis,"UTF-8");
+			br = new BufferedReader(isr);
+			int count=0;		//가장 count 높은 temp가 당첨
+			int i =0;
+			while((temp[i]=br.readLine()) != null) {
+				for (int idx = 0; idx < keyword.length; idx++) {
+					if(temp[i].contains(keyword[idx])) {
+						count++;	
+					}
+				}
+				i++;
+			}
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} finally {
+			fis.close();
+			isr.close();
+			br.close();
+		}
+		
+	}
+	
+}
